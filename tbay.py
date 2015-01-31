247d68c180a3e35c3c0809b734ed23f3021ef353
rtuita23 <rktuita23@hotmail.com>
2015-01-31 08:03:52 +0000

    created models

---
+++ tbay.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import  datetime
+from sqlalchemy import Column, Integer, String, DateTime
+
+engine = create_engine('postgresql://action:action@localhost:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    start_time = Column(DateTime, default=datetime.utcnow)
+print 'after class item'
+class User(Base):
+    __tablename__="name"
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+print 'after class item'
+
+class Bid(Base):
+    __tablename__="bid"
+    
+    id = Column(Integer, primary_key=True)
+    price = Column(Floating, nullable=False)
+    
+print 'after base'
+    
+Base.metadata.create_all(engine)
+
+print 'after Base'
+
+
