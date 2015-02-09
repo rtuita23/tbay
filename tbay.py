67fcf2edf8cb27555e0656b6c6f3275d7d8b6822
rtuita23 <rktuita23@hotmail.com>
2015-02-09 14:15:45 +0000

    added relationships

--- tbay.py
+++ tbay.py
@@ -1,13 +1,40 @@
+'''
+    MODELS RELATIONSHIPS
+    
+    users should be able to auction multiple items
+    1 user to many items relationship
+    
+    users should be able to bid on multiple items
+    many users to many items relationship
+    
+    multiple users should be able to place a bid on an item
+    1 item to many users relationship
+'''
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import  datetime
-from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 
 engine = create_engine('postgresql://action:action@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+
+print '1'
+
+class User(Base):
+    __tablename__='name'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    
+    items = relationship("Item", backref="nameitem")
+    bids = relationship('Bid', backref="namebid")
+
+print '2'
 
 class Item(Base):
     __tablename__ = "items"
@@ -16,14 +43,10 @@ class Item(Base):
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
-print 'after class item'
-class User(Base):
-    __tablename__="name"
     
-    id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-print 'after class user'
+    user_id = Column(Integer, ForeignKey('name.id'), nullable=False)
+
+print '3'    
 
 class Bid(Base):
     __tablename__="bid"
@@ -31,13 +54,22 @@ class Bid(Base):
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
     
-print 'after bid'
+    user_id = Column(Integer, ForeignKey('name.id'), nullable=False)
+    items_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    
+print '4'
     
 Base.metadata.create_all(engine)
 
-print 'last line'
+print '5'
 
-
-
-
-
+#u1 = User(username = 'Ivi', password = 'ivi')
+#mateo = User(username = 'Teo', password = 'teo')
+#baseball = Item(name='baseball', description = 'brown ball', start_time = datetime.utcnow())
+#mateo.items = [baseball]
+#ivi_bid = Bid(price=12.0, user_id = 3, items_id = 1)
+#kaho_bid = Bid(price=7.0, user_id = 2, items_id = 1)
+#fia_bid = Bid(price=8.0, user_id = 1, items_id = 1)
+#session.add_all([fia_bid, ivi_bid, kaho_bid])
+session.query(Bid.user_id).order_by(Bid.price).all()
+session.commit()
